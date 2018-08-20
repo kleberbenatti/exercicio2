ed2b2be3009a0d9e65d3454874c20f332f29fcf5
Kleber Benatti <kleber@fearp.usp.br>
2018-08-20 16:52:23 -0300

    Ajuste nas mensagens de exibição

--- calculadora.php
+++ calculadora.php
@@ -5,5 +5,5 @@ $soma = $var1 + $var2;
 $subtracao = $var1 - $var2;
 echo("Variavel 1: $var1\n");
 echo("Variavel 2: $var2\n");
-echo("A soma das variaveis 1 e 2 é: ".$soma."\n");
-echo("Subtração das variaveis: $subtracao\n");
+echo("A soma das variaveis 1 e 2 é: $soma\n");
+echo("A subtração da variável 2 em variável 1 é: $subtracao\n");
