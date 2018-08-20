00e9827946e6d6748a11e95e0850b1b8a8eb46a0
Kleber Benatti <kleber@fearp.usp.br>
2018-08-20 16:50:46 -0300

    Inclusão da multiplicao

--- calculadora.php
+++ calculadora.php
@@ -2,6 +2,8 @@
 $var1 = 10;
 $var2 = 20;
 $soma = $var1 + $var2;
+$subtracao = $var1 - $var2;
 echo("Variavel 1: $var1\n");
 echo("Variavel 2: $var2\n");
 echo("A soma das variaveis 1 e 2 é: ".$soma."\n");
+echo("Subtração das variaveis: $subtracao\n");
